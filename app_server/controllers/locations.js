494cdfe8a57d43904e1fdc224b8735acf3889a58
23eg107e34 <23eg107e34@anurag.edu.in>
2025-08-20 20:14:52 +0530

    second commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,5 @@
 const homelist = (req,res) => {
-    res.render('index', {title: 'Home'});
+    res.render('index', {title: 'Welcome Page'});
 };
 const locationInfo = (req,res) => {
     res.render('index', {title: 'LocationInfo'});
